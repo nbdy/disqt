43eea702eb741152920cd3dd42fc57260230580b
nbdy <pascal@eberlein.io>
2020-08-09 14:53:17 +0200

    update

--- disqt.h
+++ disqt.h
@@ -5,6 +5,8 @@
 #ifndef DISQT_DISQT_H
 #define DISQT_DISQT_H
 
+#include <future>
+
 #include <cpp_redis/cpp_redis>
 
 #include <QObject>
@@ -16,6 +18,8 @@ class RedisQT : public QObject {
     Q_OBJECT
     Q_PROPERTY(QString host READ getHost WRITE setHost NOTIFY hostChanged)
     Q_PROPERTY(int port READ getPort WRITE setPort NOTIFY portChanged)
+    Q_PROPERTY(bool clientIsConnected READ getClientIsConnected NOTIFY clientIsConnectedChanged)
+    Q_PROPERTY(bool subscriberIsConnected READ getSubscriberIsConnected NOTIFY subscriberIsConnectedChanged)
 
 public:
     explicit RedisQT(QObject *parent = nullptr){
@@ -29,24 +33,51 @@ public:
         qmlRegisterType<RedisQT>("io.eberlein.disqt", 1, 0, "redis");
     }
 
+    Q_INVOKABLE void connect(){
+        connect_client();
+        connect_subscriber();
+    }
+
+    Q_INVOKABLE void disconnect(){
+        disconnect_client();
+        disconnect_subscriber();
+    }
+
     Q_INVOKABLE void connect_client(){
         client.connect(host.toStdString(), port);
-        if(client.is_connected()) emit clientConnected();
+        emit clientIsConnectedChanged(client.is_connected());
+    }
+
+    Q_INVOKABLE void disconnect_client(){
+        client.disconnect();
+        emit clientIsConnectedChanged(client.is_connected());
+    }
+
+    Q_INVOKABLE void disconnect_subscriber(){
+        subscriber.disconnect();
+        emit subscriberIsConnectedChanged(subscriber.is_connected());
     }
 
     Q_INVOKABLE void connect_subscriber(){
         subscriber.connect(host.toStdString(), port);
-        if(subscriber.is_connected()) emit subscriberConnected();
+        emit subscriberIsConnectedChanged(subscriber.is_connected());
     }
 
     Q_INVOKABLE void set(const QString& key, const QString& value){
-        client.set(key.toStdString(), value.toStdString()); client.commit();
+        client.set(key.toStdString(), value.toStdString());
+        client.commit();
     }
 
-    Q_INVOKABLE void get(const QString& key){
+    Q_INVOKABLE void getAsync(const QString& key){
         client.get(key.toStdString(), [&](cpp_redis::reply& r){
             getReturned(key, QString::fromStdString(r.as_string()));
-        }); client.commit();
+        });
+    }
+
+    Q_INVOKABLE QString get(const QString& key){
+        std::future<cpp_redis::reply> r = client.get(key.toStdString());
+        r.wait();
+        return QString::fromStdString(r.get().as_string());
     }
 
     Q_INVOKABLE void subscribe(const QString& channel){
@@ -87,6 +118,9 @@ public:
 
     int getPort() const {return port;}
 
+    bool getClientIsConnected() const {return this->client.is_connected();}
+    bool getSubscriberIsConnected() const {return this->subscriber.is_connected();}
+
 signals:
     void hostChanged();
     void portChanged();
@@ -96,8 +130,8 @@ signals:
     void unsubscribed(const QString& channel);
     void psubscribed(const QString& channel);
     void punsubscribed(const QString& channel);
-    void clientConnected();
-    void subscriberConnected();
+    void clientIsConnectedChanged(bool value);
+    void subscriberIsConnectedChanged(bool value);
 
 private:
     cpp_redis::client client;
